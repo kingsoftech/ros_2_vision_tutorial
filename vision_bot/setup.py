f044aecabbb8fc66674cb9a0da3c85325233af43
kingsoftech <asoyero@student.lautech.edu.ng>
2024-02-18 16:40:47 +0100

    meshes update

--- vision_bot/setup.py
+++ vision_bot/setup.py
@@ -12,12 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name,'meshes'),glob('meshes/*'))
-    ],
+        (os.path.join('share', package_name,'meshes'),glob('meshes/*')),
+       (os.path.join('share', package_name,'urdf'),glob('urdf/*')) ,
+    (os.path.join('share', package_name,'launch'),glob('launch/*'))]
+  ,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='abdullah',
-    maintainer_email='asoyero@student.lautech.edu.ng',
+    maintainer_email='asoyero@student.h.lautech.edu.ng',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
