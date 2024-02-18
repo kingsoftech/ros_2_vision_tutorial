4c2706c62a67d556f9c98990b66408c23b84cab8
kingsoftech <asoyero@student.lautech.edu.ng>
2024-02-18 20:40:20 +0100

    still on meshes

--- vision_bot/launch/rviz.launch.py
+++ vision_bot/launch/rviz.launch.py
@@ -1,11 +1,13 @@
+from webbrowser import get
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
     package_share_dir = get_package_share_directory("vision_bot")
-    urdf_file_path = os.path.join(package_share_dir, "urdf", "vision_rpi_bot.urdf")
+    urdf_file_path = os.path.join(package_share_dir, "urdf","vision_rpi_bot.urdf")
 
     return LaunchDescription([
         Node(
