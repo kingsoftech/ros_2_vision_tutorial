cc3649f45cc794e9587d2cb16b939f4acee7f550
kingsoftech <asoyero@student.lautech.edu.ng>
2024-03-01 07:45:05 +0100

    comment

--- vision_bot/scripts/qr-code.py
+++ vision_bot/scripts/qr-code.py
@@ -1,10 +1,19 @@
 import cv2
-
+#fetching the image directory
 img = cv2.imread('/home/abdullah/ros2_workspace/src/ros_2_vision_tutorial/vision_bot/meshes/left.png')
+
+#creating an opencv object decoder
 decoder = cv2.QRcodeDetector()
+
+#getting the object and data
 data, points, _ = decoder.detectAndDecode(img)
 
+#printing out the result
 print("results = ", data)
+#viewing the image
 cv2.imshow("Detected QR code is, ", img)
+#initializing press any key to exit
 cv2.waitKey()
+
+#destroys all opened windows when exited
 cv2.destroyAllWindows()
