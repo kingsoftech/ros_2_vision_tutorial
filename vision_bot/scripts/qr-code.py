156be2a4e94d76bc664cc5de4a402f07a764bf02
kingsoftech <asoyero@student.lautech.edu.ng>
2024-03-02 21:26:41 +0100

    maze_qr_update

--- vision_bot/scripts/qr-code.py
+++ vision_bot/scripts/qr-code.py
@@ -1,19 +1,29 @@
+from pyzbar.pyzbar import decode
 import cv2
-#fetching the image directory
+
+# Fetching the image directory
 img = cv2.imread('/home/abdullah/ros2_workspace/src/ros_2_vision_tutorial/vision_bot/meshes/left.png')
 
-#creating an opencv object decoder
-decoder = cv2.QRcodeDetector()
+# Converting the image to grayscale
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#getting the object and data
-data, points, _ = decoder.detectAndDecode(img)
+# Getting the decoded objects and data
+decoded_objs = decode(gray_img)
 
-#printing out the result
-print("results = ", data)
-#viewing the image
-cv2.imshow("Detected QR code is, ", img)
-#initializing press any key to exit
-cv2.waitKey()
+# Looping through the decoded objects
+for obj in decoded_objs:
+    data = obj.data.decode('utf-8')
+    print("Result:", data)
+
+# Displaying the image
+cv2.imshow("Detected QR code", img)
+
+# Initializing press any key to exit
+cv2.waitKey(0)
+
+# Destroys all opened windows when exited
+cv2.destroyAllWindows()
+
 
 #destroys all opened windows when exited
 cv2.destroyAllWindows()
