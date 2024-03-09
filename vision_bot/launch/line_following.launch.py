e172a9a60a5c379e0630acf7ed26ed2df956cfba
kingsoftech <asoyero@student.lautech.edu.ng>
2024-03-09 22:04:59 +0100

    following_line

--- vision_bot/launch/line_following.launch.py
+++ vision_bot/launch/line_following.launch.py
@@ -4,10 +4,11 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     package_share_dir = get_package_share_directory("vision_bot")
     urdf_file_path = os.path.join(package_share_dir, "urdf","vision_rpi_bot.urdf")
-    world_file_path = os.path.join(package_share_dir, "worlds","qr_maze.world")
+    world_file_path = os.path.join(package_share_dir, "worlds","lineFollow.world")
     urdf = open(urdf_file_path).read()
 
 
@@ -18,6 +19,17 @@ def generate_launch_description():
                 output="screen",
             ),
 
-       
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                parameters=[{'robot_description': urdf}],
+            ),
+            Node(
+                package="gazebo_ros",
+                executable="spawn_entity.py",
+                arguments=["-topic","robot_description",
+                           "-entity","vision_bot"],#,"-Y","1.57"],
+                output= 'screen'
+            ),
         ]
     )
